b6edc663a088f2a1b3855517d418d6fa16b0505c
Danila Mishin <danila.mishin.2001@gmail.com>
2020-12-25 16:21:13 +0300

    Refactored

---
+++ tupper_decoder.py
@@ -0,0 +1,71 @@
+from PIL import Image, ImageDraw
+
+from tupper_constants import IMAGE_MODE, WIDTH, HEIGHT, \
+    AREA, SIZE_TUPLE, DEFAULT_IMAGE_COLOR
+
+class TapperDecoder:
+    def _height_to_bin(self, height: int) -> str:
+        """
+        This is the first step of reverse Tapper's algo
+        We take the height (mentioned as 'k' in the article)
+        and convert it into binary form after dividing it by 17
+
+        :param: height - Height of the image
+        :return: String of binary form
+        """
+
+        height //= 17
+
+        bin_tail = bin(height)[2:]
+        remain = (AREA - len(bin_tail))
+        binary = ('0' * remain) + bin_tail
+
+        return binary
+
+    def _bin_to_image_lists(self, binary: str) -> list:
+        """
+        This is the second step of revers Tapper's algo
+        We look at the binary form of the image height and try
+        to decode it.
+
+        :param: binary - Binary form of the height
+        :return: Image form of lists
+        """
+
+        image_lists = [list() for x in range(HEIGHT)]
+
+        for i in range(AREA):
+            current_row = -(i % HEIGHT)
+            image_lists[current_row].append(binary[i])
+
+        return image_lists
+    
+
+    def generate_image(self, height: int) -> Image:
+        """
+        This method generates image from its height using
+        Tapper's formula
+
+        :param: height - Image height
+        :return: PIL image object
+        """
+
+        binary = self._height_to_bin(height)
+        image_lists = self._bin_to_image_lists(binary)
+
+        image = Image.new(
+            IMAGE_MODE, 
+            SIZE_TUPLE, 
+            DEFAULT_IMAGE_COLOR
+        )
+
+        for y in range(HEIGHT):
+            for x in range(WIDTH):
+                pixel_value = (int(image_lists[y][x],))
+
+                image.putpixel(
+                    xy=(WIDTH - x - 1, HEIGHT - y - 1),
+                    value=pixel_value
+                )
+
+        return image
---
+++ tupper_encoder.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw
+
+from tupper_constants import IMAGE_MODE, WIDTH, HEIGHT, \
+    AREA, SIZE_TUPLE, TWO_COLOR_MODE, BLACK_COLOR_CODE
+
+
+class TupperEncoder:
+    def get_height(self, image_path: str) -> int:
+        """
+        This method return code of the image using
+        Tupper's formula
+
+        :param: image_path - Path of the image
+        :return: Tupper's height 
+        """
+
+        image = Image.open(image_path)
+
+        height, width = image.size
+        if width == WIDTH and height == HEIGHT:
+            raise Exception("Wrong size. Must be {0}x{1}".format(WIDTH, HEIGHT))
+
+        image = image.convert(TWO_COLOR_MODE)
+        binary = str()
+
+        for x in range(WIDTH - 1, -1, -1):
+            for y in range(HEIGHT):
+                binary += '1' if image.getpixel((x, y)) > BLACK_COLOR_CODE else '0'
+
+        height = int(binary, 2) * 17
+        return height
